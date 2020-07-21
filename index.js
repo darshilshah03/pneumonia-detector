7a5abbe4fb3525d216a9ffb0cf94035840387375
Darshil Shah <shah.darshil23@yahoo.com>
2020-07-21 18:45:17 +0530

    first commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 
-app.get('/', (req,res)=>{
+app.get('/https://darshilshah03.github.io/pneumonia-detector/', (req,res)=>{
     res.render('index')
 })
 
